f4a99a5b29cc16c87b15a5069733ce8a8b270909
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 23:34:42 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,29 +1,27 @@
 class Doctor
   attr_accessor :name
-
+  
   @@all = []
-
+  
   def initialize(name)
     @name = name
     @@all << self
   end
-
+  
   def self.all
     @@all
   end
-
-  def appointments
-    Appointment.all.collect do |appointment|
-      if appointment.doctor == self
-        true
-      else
-        false
-      end
-    end
-  end
-
+  
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
-
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def patients
+    self.appointments.collect {|appointment| appointment.patient}
+  end
+  
 end

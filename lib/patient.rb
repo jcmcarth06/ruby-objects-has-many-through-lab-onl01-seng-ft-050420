bfe1973caf3406b39c76f2e2509ae0b716d6de43
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 23:33:42 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,5 @@
 class Patient
+
   attr_accessor :name, :appointment, :doctor
 
   @@all = []
@@ -8,12 +9,20 @@ class Patient
     @@all << self
   end
 
-  def new_appointment(date, doctor)
-    Appointment.new (date, self, doctor)
-    end
-  end
-
   def self.all
     @@all
   end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
 end
